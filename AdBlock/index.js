4c749217512f7fcb4bcc0fcecf4e5868dfcd9506
twnlink <twnlink@users.noreply.github.com>
2023-07-18 16:11:55 +0000

    deploy: 116cd47f507402ccb5c42440731f1d49f28b4128

---
+++ AdBlock/index.js
@@ -0,0 +1,1 @@
+import{appendStyle as a}from"@neptune/utils";import{intercept as s}from"@neptune";var t=[];t.push(s(["interruptions/QUEUE_INTERRUPTION","interruptions/ADD_ITEMS_TO_INTERRUPTIONS_ARRAY"],()=>!0),a('[class*="sidebarUpgrade--"],[data-test^="streaming-audio-quality-"][class*="selectItemIsDisabled--"]{display:none!important}'));var n=()=>t.forEach(e=>e());export{n as onUnload};
